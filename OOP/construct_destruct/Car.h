071923bb396cf1fd64cea76839a9d3fecf147c8a
dgalytskyi <galitskiy.dima8@gmail.com>
2024-05-27 19:13:48 +0300

    Added copy constructor

--- OOP/construct_destruct/Car.h
+++ OOP/construct_destruct/Car.h
@@ -81,6 +81,25 @@ namespace CPP {
                  << endl;
         }
 
+        Car(const Car &object) {
+            char *to_insert;
+
+            to_insert = new char[strlen(object.model) + 1];
+            strcpy(to_insert, object.model);
+            this->model = to_insert;
+
+            to_insert = new char[strlen(object.country) + 1];
+            strcpy(to_insert, object.country);
+            this->country = to_insert;
+
+            to_insert = new char[strlen(object.color) + 1];
+            strcpy(to_insert, object.color);
+            this->color = to_insert;
+
+            this->year = object.year;
+            this->price = object.price;
+        }
+
         ~Car() {
             delete[] this->model;
             delete[] this->country;
