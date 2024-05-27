b6c658d6b0bee014f9344446cc8a83c51e3dd00e
dgalytskyi <galitskiy.dima8@gmail.com>
2024-05-27 19:14:13 +0300

    Added copy constructor tests and deleted old tests

--- main.cpp
+++ main.cpp
@@ -4,9 +4,8 @@ using CPP::Car;
 
 int main() {
     Car car1;
-    Car car2("Toyota", "Japan", "Purple", 2007, 1233.1);
+    Car car2 = car1;
 
-    car1.input();
     car1.print();
     car2.print();
 }
