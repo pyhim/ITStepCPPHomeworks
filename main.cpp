0b08136140343958d665db55b15869ef996ce454
dgalytskyi <galitskiy.dima8@gmail.com>
2024-06-01 16:16:49 +0300

    tests rewritten

--- main.cpp
+++ main.cpp
@@ -3,23 +3,13 @@
 using namespace CPP;
 
 int main() {
-    const size_t FLATS = 2, PERSONS = 2;
-
-    Person family1[PERSONS]{
-            Person("123412312", "Dmytro", "Galytskyi", "Ruslanovych", Date(14, 10, 2006)),
-            Person("123412312", "Dmytro", "Galytskyi", "Ruslanovych", Date(14, 10, 2006))
-    };
-    Person family2[PERSONS]{
-            Person("123412312", "Dmytro", "Galytskyi", "Ruslanovych", Date(14, 10, 2006)),
-            Person("123412312", "Dmytro", "Galytskyi", "Ruslanovych", Date(14, 10, 2006))
-    };
-    Flat flats[FLATS]{
-            Flat(family1, PERSONS),
-            Flat(family2, PERSONS)
-    };
-    FlatHouse house(flats, FLATS);
-
-    house.getFlat(0).getFamily()[0].print_credentials();
+    Person person1("123412312", "Dmytro", "Galytskyi", "Ruslanovych", Date(14, 10, 2006));
+    Person person2("123412312", "Dmytro", "Galytskyi", "Ruslanovych", Date(14, 10, 2006));
+    Person family[2]{person1, person2};
+    Flat flat1(family, 2, 15.1);
+    Flat flat2(family, 2, 16.2);
+    Flat flats[2]{flat1, flat2};
+    FlatHouse house(flats, 2);
 
     return 0;
 }
