6b82f4d483d7445b83639922d7444c6a9a4b463a
dgalytskyi <galitskiy.dima8@gmail.com>
2024-06-05 16:00:59 +0300

    added reservoir test

--- main.cpp
+++ main.cpp
@@ -1,15 +1,9 @@
-#include "OOP/FlatHouse/headers/FlatHouse.h"
+#include "OOP/Reservoir/reservoir_test.h"
 
 using namespace CPP;
 
 int main() {
-    Person person1("123412312", "Dmytro", "Galytskyi", "Ruslanovych", Date(14, 10, 2006));
-    Person person2("123412312", "Dmytro", "Galytskyi", "Ruslanovych", Date(14, 10, 2006));
-    Person family[2]{person1, person2};
-    Flat flat1(family, 2, 15.1);
-    Flat flat2(family, 2, 16.2);
-    Flat flats[2]{flat1, flat2};
-    FlatHouse house(flats, 2);
+    reservoir_test();
 
     return 0;
 }
