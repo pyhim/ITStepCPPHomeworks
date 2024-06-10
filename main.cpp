eba597487310fc4f851017d093415ee5bfcc0ba5
dgalytskyi <galitskiy.dima8@gmail.com>
2024-06-10 22:25:21 +0300

    cleared

--- main.cpp
+++ main.cpp
@@ -1,16 +1,8 @@
 #include <iostream>
-#include "OOP/Fraction/Fraction.h"
 
-using namespace CPP;
-using std::cout;
+using namespace std;
 
-int main() {
-    Fraction fraction1(5, 3), fraction2(2, 3);
+int main()
+{
 
-    (fraction1 + fraction2).print();
-    (fraction1 - fraction2).print();
-    (fraction1 * fraction2).print();
-    (fraction1 / fraction2).print();
-
-    return 0;
 }
