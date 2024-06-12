65eb7a8c70015c68d2650f5401b19b5414f3522f
dgalytskyi <galitskiy.dima8@gmail.com>
2024-06-12 19:10:39 +0300

    test for Car

--- main.cpp
+++ main.cpp
@@ -1,12 +1,28 @@
-#include "OOP/MyString/MyString.h"
+#include <iostream>
+#include "OOP/Car/Car.h"
 
 using namespace CPP;
+using namespace std;
 
 int main() {
-    MyString str("hello precious wonderful world!");
+    Car car1("Audi", 2023, 50000), car2("BMW", 2019, 30000);
+    
+    cout << car1 << endl;
+    cin >> car1;
+    cout << car1 << endl;
+    
+	car1 += 1000; // увеличиваем стоимость на 1000
+    cout << car1;
+	car1 -= 1000; // уменьшаем стоимость на 1000
+    cout << car1;
 
-    str -= 'h';
-    str.output();
-    str += " I said hello world!";
-    str.output();
+	++car1; // увеличиваем год на 1
+    cout << car1;
+	--car1; // уменьшаем год на 1
+    cout << car1;
+
+	double r = car1 - car2; // разность стоимости автомобилей
+	double sum = car1 + car2; // сумма стоимости автомобилей , возвращается общая сумма
+    
+    cout << r << ' ' << sum << endl;
 }
