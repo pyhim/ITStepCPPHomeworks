0cde92210916c137507107f82b4a5115662bfa60
dgalytskyi <galitskiy.dima8@gmail.com>
2024-06-24 20:13:09 +0300

    tests for Printer

--- main.cpp
+++ main.cpp
@@ -1,36 +1,17 @@
 #include <iostream>
-#include "OOP/Car/Car.h"
+#include "OOP/Printer/Printer.h"
 
 using namespace CPP;
 using namespace std;
 
 int main() {
-    Car car1("Audi", "Red", "Germany", 2023, 50000);
-    Car car2("BMW", "Blue", "Germany", 2019, 30000);
-    
-    /*cout << car1 << endl;
-    cin >> car1;
-    cout << car1 << endl;
-    
-	car1 += 1000; // увеличиваем стоимость на 1000
-    cout << car1;
-	car1 -= 1000; // уменьшаем стоимость на 1000
-    cout << car1;
+    Printer printer;
 
-	++car1; // увеличиваем год на 1
-    cout << car1;
-	--car1; // уменьшаем год на 1
-    cout << car1;
+    printer.addToPrintQueue("sadasdasd", "something");
+    printer.addToPrintQueue("sfgfsdg", "somefgdfsg");
+    printer.addToPrintQueue("shrhrthtr", "sdfgfdgthing");
 
-	double r = car1 - car2; // разность стоимости автомобилей
-	double sum = car1 + car2; // сумма стоимости автомобилей , возвращается общая сумма
-    
-    cout << r << ' ' << sum << endl;*/
+    printer.print();
 
-    Car car3 = std::move(car1), car4("Nissan", "Gray", "Japan",1231, 124123);
-    cout << car3;
-    cout << car4;
-
-    car4 = std::move(car2);
-    cout << car4;
+    printer.print_statistic();
 }
